b94ebafb5314e7374b00a3dfba5ecab3f12199ce
GUY Jérôme <scoonydeus@gmail.com>
2018-07-24 22:31:17 +0200

    Update super-save.sh

--- super-save.sh
+++ super-save.sh
@@ -7,20 +7,23 @@ requested: git-core
 #### Tautulli (previously plexpy)
 if [[ "$1" == "--restore" ]]; then
   if [[ -d "/mnt/save/..." ]]; then
-    ## peut etre une question pour savoir si on restore
-    if [[ -d "/opt/Tautulli" ]]; then
-      cp /mnt/save...
-    else
-      cd /opt
-      git clone https://github.com/Tautulli/Tautulli.git
-      adduser --system --no-create-home tautulli
-      chown tautulli:nogroup -R /opt/Tautulli
-      touch /etc/default/tautulli
-      chmod +x /opt/Tautulli/init-scripts/init.ubuntu
-      cp /opt/Tautulli/init-scripts/init.ubuntu /etc/init.d/tautulli
-      update-rc.d tautulli defaults
-      cp /mnt/save...
-      service tautulli start
+    printf "Do you want to restore/install Tautulli? (yes/no) : "
+    read question_tautulli
+    if [[ "${question_tautulli,,}" == "yes|y" ]]; then
+      if [[ -d "/opt/Tautulli" ]]; then
+        cp /mnt/save...
+      else
+        cd /opt
+        git clone https://github.com/Tautulli/Tautulli.git
+        adduser --system --no-create-home tautulli
+        chown tautulli:nogroup -R /opt/Tautulli
+        touch /etc/default/tautulli
+        chmod +x /opt/Tautulli/init-scripts/init.ubuntu
+        cp /opt/Tautulli/init-scripts/init.ubuntu /etc/init.d/tautulli
+        update-rc.d tautulli defaults
+        cp /mnt/save...
+        service tautulli start
+      fi
     fi
   fi
 else
