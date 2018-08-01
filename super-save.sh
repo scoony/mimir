6db2b6f26f677bc4bcb8239d874cfea72a6d5fa0
GUY Jérôme <scoonydeus@gmail.com>
2018-08-01 19:19:28 +0200

    Update super-save.sh

--- super-save.sh
+++ super-save.sh
@@ -139,6 +139,15 @@ done
 
 
 
+
+
+
+#### MOTD du SSH
+cd /etc/update-motd.d
+custom_motd=`ls -I 00-header -I 10-help-text -I 50-motd-news -I 80-esm -I 80-livepatch -I 90-updates-available -I 91-release-upgrade -I 95-hwe-eol -I 98-fsck-at-reboot -I 98-reboot-required`
+
+
+
 #### Tautulli (anciennement plexpy)
 if [[ "$1" == "--restore" ]]; then
   if [[ -d "/mnt/save/..." ]]; then
