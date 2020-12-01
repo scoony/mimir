0a1325255f93c36b5d1f34650b57f3b839fc3dc6
FRONTEAU Adrien <zouzou.is.reborn@hotmail.fr>
2020-12-01 09:25:10 +0100

    Oups

--- extras/installer.sh
+++ extras/installer.sh
@@ -47,7 +47,7 @@ while kill -0 $pid 2>/dev/null; do
   sleep .1
 done
 chmod +x "$install_path/mimir.sh"
-if [[ !-d "/root/.config/mimir/MUI" ]]; then mkdir -p "/root/.config/mimir/MUI"; fi
+if [[ ! -d "/root/.config/mimir/MUI" ]]; then mkdir -p "/root/.config/mimir/MUI"; fi
 wget -q "$remote_folder/MUI/$os_language.lang" -O "/root/.config/mimir/MUI/$os_language.lang" >> $log_install &
 pid=$!
 spin='-\|/'
